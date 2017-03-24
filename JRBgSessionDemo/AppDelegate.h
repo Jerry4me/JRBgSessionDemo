e3ecb49c19e109eddb1d663e1207a716b830892a
Jerry4me <bestjerry4me@gmail.com>
2017-03-24 13:44:57 +0800

    Initial Commit

---
+++ JRBgSessionDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  JRBgSessionDemo
+//
+//  Created by sky on 2017/3/24.
+//  Copyright © 2017年 sky. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ JRBgSessionDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  JRBgSessionDemo
+//
+//  Created by sky on 2017/3/24.
+//  Copyright © 2017年 sky. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
